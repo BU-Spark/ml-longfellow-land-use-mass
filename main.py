72386f655db577361d55412d798c1c6d3082c84c
Vijay Fisch <129314650+vijayfisch@users.noreply.github.com>
2024-03-13 11:09:44 -0400

    land use mass: Update files

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from modules.racist_chatgpt_analysis import racist_chatgpt_analysis
 from modules.locate import locate
 import os
 
-def racism_threshold(file_dir):
+def make_csv(file_dir):
   for images in os.listdir(file_dir):
     if images.endswith(".tif"):
       text= tiff_to_ocr(images)
@@ -15,10 +15,9 @@ def racism_threshold(file_dir):
       result2 = racist_text_query(text, bigotry_dict)
 
       if result1 or result2:
-        a,b = locate(text)
-        print(a,b)
-      else:
-        a,b = locate(text)
-        print(a,b)
+        page,date = locate(text)
+        print(page,date)
 
-racism_threshold('/Users/vijayfisch/Desktop/DD4G 2024 Land Use')
+
+
+make_csv('/Users/vijayfisch/Desktop/DD4G 2024 Land Use/dd4g-land-use-mass')
