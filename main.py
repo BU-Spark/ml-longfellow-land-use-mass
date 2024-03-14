38f40a4a9a2fbc14736a2bc9176c56e95651b7c0
Naman Nagaria <144247530+namanhash@users.noreply.github.com>
2024-03-14 11:50:44 -0400

    land use mass: final main

--- main.py
+++ main.py
@@ -1,36 +1,64 @@
-# from modules import bigotry_dict, check_content, check_racial_content, read_text
 from modules.racist_text_query import racist_text_query
 from modules.bigotry_dict import bigotry_dict
 from modules.OCR import tiff_to_ocr
 from modules.racist_chatgpt_analysis import racist_chatgpt_analysis
 from modules.locate import locate
+from modules.pagenum import crop_image
 import os
 import pandas as pd
 
 def racism_threshold(file_dir):
-  data =[]
-  for images in os.listdir(file_dir):
-    if images.endswith(".tif") or images.endswith(".tiff"):
-      text= tiff_to_ocr(images)
+    # Create the new folder for cropped images
+    cropped_images_dir = os.path.join(file_dir, 'deed page number')
+    if not os.path.exists(cropped_images_dir):
+        os.makedirs(cropped_images_dir)
 
-      result1 = racist_chatgpt_analysis(text)
-      result2 = racist_text_query(text, bigotry_dict)
+    data = []
+    for images in os.listdir(file_dir):
+        if images.endswith(".tif") or images.endswith(".tiff"):
+            image_path = os.path.join(file_dir, images)
 
-      if result1 or result2:
-        a,b,c = locate(text)
-        print(images,a,b,c)
-        data.append([images,a[0], b[0], c[0]])
-      else:
-        print(images  + " : Not Racist")
-        a,b,c = locate(text)
-        data.append([images,a[0], b[0], c[0]])
+            # run ocr on images
+            text = tiff_to_ocr(image_path)
 
-      
-  #print(data)
-  df = pd.DataFrame(data, columns = ['File name', 'Page Number', 'Date', 'Book Number'])
-  df.index += 1
-  df.to_csv('Racist Deeds.csv', index = True)
-  #print(df)
-        
+            result1 = racist_chatgpt_analysis(text)
+            result2 = racist_text_query(text, bigotry_dict)
 
-racism_threshold('/Your/Path/To/Files')
+            a, b, c = locate(text)
+            
+            # Define the output path for the cropped image in the new folder
+            cropped_image_name = "cropped_" + images
+            cropped_image_path = os.path.join(cropped_images_dir, cropped_image_name)
+            
+            # Crop the image and save it to the new folder
+            crop_image(image_path, cropped_image_path)
+            
+            image_path_formatted = cropped_image_path
+            #.replace(' ', '%20')
+            hyperlink_formula = f'file://{image_path_formatted}'
+            
+            # fail safe page number detection 
+            page = tiff_to_ocr(cropped_image_path)
+            fail_safe_page = []
+            result = page.split("\n")
+            for word in result:
+              # checks for possible page numbers
+              if word.isdigit() == True:
+                fail_safe_page.append(word)
+            
+            
+            if result1 or result2:
+                print(images, a, b, c)
+                if len(fail_safe_page) != 0:
+                    a.append(fail_safe_page)
+                data.append([images, a, b[0], c[0], hyperlink_formula])
+            else:
+                print(images + " : Not Racist")
+                data.append([images, a, b[0], c[0], hyperlink_formula])
+
+    # Include the hyperlink in the DataFrame columns
+    df = pd.DataFrame(data, columns=['File Name', 'Probable Page Number', 'Date', 'Book Number', "Page Link"])
+    df.index += 1
+    df.to_csv(os.path.join(file_dir, 'Racist Deeds.csv'), index=True)
+
+racism_threshold('/Users/namannagaria/Desktop/new/dd4g-land-use-mass')
