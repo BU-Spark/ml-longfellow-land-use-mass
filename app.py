690eea123dac3da2525b1742f7007547df83d100
jacob-stein1 <jmstein@bu.edu>
2024-10-03 14:57:46 -0400

    Finalize Azure OCR in Flask server
    Co-authored-by: Jianying Liu <jianyingx12@gmail.com>

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import io
 from modules.google_cloud_ocr.google_cloud_ocr import google_cloud_ocr
+from modules.azure_cloud_ocr.azure_cloud_ocr import azure_cloud_ocr
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -23,7 +24,7 @@ def upload_file():
             google_text = google_cloud_ocr(file)
             return jsonify({'status': 'success', 'ocr_engine': 'google', 'text': google_text}), 200
         elif ocr_engine == 'azure':
-            azure_text = "filler"
+            azure_text = azure_cloud_ocr(file)
             return jsonify({'status': 'success', 'ocr_engine': 'azure', 'text': azure_text}), 200
         else:
             return jsonify({'error': 'Unsupported OCR engine selected'}), 400
