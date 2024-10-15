fe0a262fa7bf4aa72bede53df712f7a68df99ce6
jianying <jianying@bu.edu>
2024-10-15 10:49:30 -0400

    added spellchecker to modules

--- modules/deed_preprocessing/read_tiffs.py
+++ modules/deed_preprocessing/read_tiffs.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import importlib.util
+from spellcheck import correct_spelling
 
 spec = importlib.util.spec_from_file_location("google_cloud_ocr", "../google_cloud_ocr/google_cloud_ocr.py")
 google_cloud_ocr_module = importlib.util.module_from_spec(spec)
@@ -23,14 +24,15 @@ for root, dirs, files in os.walk('./tiffs'):
                 try:
                     print(tiff_file_path)
                     extracted_text = google_cloud_ocr_module.google_cloud_ocr(tiff_file)
+
+                    # spell check the extracted text
+                    corrected_text = correct_spelling(extracted_text)
                     
                     output_file_name = f"{os.path.splitext(file)[0]}.txt"
                     output_file_path = os.path.join(output_dir, output_file_name)
                     
                     with open(output_file_path, 'w', encoding='utf-8') as output_txt:
                         output_txt.write(extracted_text)
-                    
-                    print(f"OCR result for {file} saved to {output_file_name}")
                 
                 except Exception as e:
                     print(f"Error processing {file}: {str(e)}")
