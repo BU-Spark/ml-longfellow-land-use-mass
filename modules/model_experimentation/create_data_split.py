c9d07151fb4755d39f6e655bfc9de0cd5a8184b7
jacob-stein1 <jmstein@bu.edu>
2024-10-24 18:49:12 -0400

    Bag of words and logistic regression model

--- modules/model_experimentation/create_data_split.py
+++ modules/model_experimentation/create_data_split.py
@@ -8,6 +8,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 sys.path.append('../deed_preprocessing')
 from preprocessor import preprocess_text
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 def preprocess_bag_of_words(preprocessed_text_list):
     texts = [preprocessed["original_text"] for preprocessed in preprocessed_text_list]
@@ -28,8 +30,11 @@ def preprocess_deeds():
 
     all_data = pd.DataFrame()
 
+    racist_count = 0
+    non_racist_count = 0
+
     def process_directory(directory, is_racist_label):
-        nonlocal all_data
+        nonlocal all_data, racist_count, non_racist_count
         for file in directory.iterdir():
             if file.is_file() and file.suffix == '.txt':
                 with file.open('r', encoding='utf-8') as f:
@@ -41,8 +46,16 @@ def preprocess_deeds():
 
                     all_data = pd.concat([all_data, df], ignore_index=True)
 
+                if is_racist_label == 1:
+                    racist_count += 1
+                else:
+                    non_racist_count += 1
+
     process_directory(racist_dir, 1)
-    # process_directory(non_racist_dir, 0)
+    process_directory(non_racist_dir, 0)
+
+    print(f"Number of racist text files read: {racist_count}")
+    print(f"Number of non-racist text files read: {non_racist_count}")
 
     return all_data
 
@@ -53,8 +66,9 @@ if __name__ == "__main__":
     bow_df = preprocess_bag_of_words(preprocessed_text_list)
     
     X = bow_df
+    print("Bag-of-Words DataFrame dimensions:", X.shape)
     y = preprocessed_data['is_racist']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     logistic_model = LogisticRegression(max_iter=1000)
     logistic_model.fit(X_train, y_train)
     y_pred = logistic_model.predict(X_test)
@@ -63,3 +77,12 @@ if __name__ == "__main__":
     print(f"Accuracy: {accuracy:.2f}")
     print("\nClassification Report:")
     print(classification_report(y_test, y_pred))
+
+    X_test_indices = X_test.index
+    texts_test = preprocessed_data.loc[X_test_indices, 'original_text'].reset_index(drop=True)
+
+    with open('classified_racist_deeds.txt', 'w', encoding='utf-8') as f:
+        for i, text in enumerate(texts_test):
+            if y_pred[i] == 1:
+                f.write(text + '\n')
+                f.write('________________________________________\n')
